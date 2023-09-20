a6a70b3a178faa4d40fc892782d0480c186279e4
Débora Mazzitelli <115846315+debora662@users.noreply.github.com>
2023-09-20 01:01:44 -0300

    updates tailwind & js

--- carrito.js
+++ carrito.js
@@ -1,26 +1,56 @@
-let carrito = "";
-let totalCarrito = 0;
+let carrito = [];
 
-function agregarProducto(item, precio) {
-    carrito += item + "\n";
-    totalCarrito += precio; 
-    alert("Se ha agregado " + item + " al carrito. Total: $" + totalCarrito);
+const productos = [{ nombre: "Auriculares", precio: 3200, subTotal: 3200, cantidad: 1 },
+{ nombre: "Mouse", precio: 3000, subTotal: 3000, cantidad: 1 },
+{ nombre: "Teclado", precio: 3900, subTotal: 3900, cantidad: 1 },
+{ nombre: "WebCam", precio: 4200, subTotal: 4200, cantidad: 1 },
+{ nombre: "Parlantes", precio: 4800, subTotal: 4800, cantidad: 1 }]
+
+function agregarProducto(itemNombre) {
+
+    const enCarrito = carrito.find((el) => el.nombre === itemNombre);
+
+    if (enCarrito) {
+        enCarrito.cantidad++;
+        enCarrito.subTotal = enCarrito.cantidad * enCarrito.precio;
+    } else {
+        const producto = productos.find((el) => el.nombre === itemNombre);
+        if (producto) {
+            carrito.push({ ...producto });
+        }
+    }
+    alert("El producto " + itemNombre + " fue agregado con éxito");
 }
 
 function mostrarCarrito() {
-    if (carrito == "") {
+
+    if (carrito.length === 0) {
         alert("El carrito está vacio.")
     } else {
-        alert("Contenido del carrito: \n" + carrito + "\n Total: $" + totalCarrito)
+        let resumenCarrito = "CONTENIDO DEL CARRITO: \n\n";
+
+        carrito.forEach((item) => {
+            resumenCarrito += "Producto: " + item.nombre + "\nCantidad: " + item.cantidad + "\nSubtotal: $" + item.subTotal + "\n\n";
+        })
+
+        const total = carrito.reduce((acc, item) => acc + item.subTotal, 0);
+        resumenCarrito += "TOTAL DEL CARRITO: $" + total;
+
+        alert(resumenCarrito)
     }
 }
 
-function vaciarCarrito() {
-    carrito = "";
-    totalCarrito = 0;
-    alert("Se ha vaciado el carrito.")
-}
+function eliminarProducto() {
+    const nombreProducto = prompt("¿Qué producto desea eliminar?");
 
+    const producto = carrito.find((el) => el.nombre === nombreProducto);
+
+    if (nombreProducto) {
+        const indiceProducto = carrito.indexOf(producto);
+        carrito.splice(indiceProducto, 1);
+        alert("El producto " + nombreProducto + " fue eliminado.")
+    }
+}
 
 function comprar() {
     let seguirComprando = true
@@ -44,3 +74,6 @@ function comprar() {
         }
     }
 }
+
+
+
