393c3d7dd133793b35f330d0718bebf43cf1c29d
Débora Mazzitelli <115846315+debora662@users.noreply.github.com>
2023-10-18 11:34:12 -0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./**/*.{html,js}",
+export const content = ["./**/*.{html,js}",
   "./node_modules/tw-elements/dist/js/**/*.js"
-],
-  theme: {
-    extend: {
-      margin: {
-        '11': '6.5rem',              
-      },
-      width: {
-        'w-98': '390px',
-      },
-      screens: {
-        'xs': '320px',
-      },
+];
+export const theme = {
+  extend: {
+    margin: {
+      '11': '6.5rem',
+    },
+    width: {
+      'w-98': '390px',
+    },
+    screens: {
+      'xs': '320px',
     },
   },
-  plugins: [require("tw-elements/dist/plugin.cjs")],  
-}
+};
+export const plugins = [require("tw-elements/dist/plugin.cjs")];
 
